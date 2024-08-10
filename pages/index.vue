fedfb1088ff3d1709ab8730aa50d7e8f946f04ff
humbertoluks <luksprogexpert@gmail.com>
2024-08-10 17:34:25 -0300

    add NuxtLayout and NuxtPage

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="flex items-center justify-between mb-10">
+      <h1 class="text-4xl font-extrabold">
+        Summary
+      </h1>
+       <UButton color="white" variant="solid" label="My First Button" />
+    </section>
+  </template>
